b870f86576d3d68983983f407e91010d96d10488
Brandon Andre <andr0284@algonquinlive.com>
2022-02-03 13:58:15 -0500

    - Removed the second slash from the OpenAPIConstrants SPEC_PATH.

--- src/test/java/ca/gc/aafc/transaction/api/openapi/OpenAPIConstants.java
+++ src/test/java/ca/gc/aafc/transaction/api/openapi/OpenAPIConstants.java
@@ -3,7 +3,7 @@ package ca.gc.aafc.transaction.api.openapi;
 public final class OpenAPIConstants {
 
   static final String SPEC_HOST = "raw.githubusercontent.com";
-  static final String SPEC_PATH = "DINA-Web//loan-transaction-specs/master/schema/transaction-api.yml";
+  static final String SPEC_PATH = "DINA-Web/loan-transaction-specs/master/schema/transaction-api.yml";
 
   private OpenAPIConstants() {
   }
